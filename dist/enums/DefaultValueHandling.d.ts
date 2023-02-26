107a66650b53d4aaf0fe4fa08e2280daa1b7eabc
Hannes Rüger <hannesrueger@gmx.de>
2023-02-26 10:07:03 +0100

    fix: remove dist

--- dist/enums/DefaultValueHandling.d.ts
+++
@@ -1,6 +0,0 @@
-export declare enum DefaultValueHandling {
-    Include = 0,
-    Ignore = 1,
-    Populate = 2,
-    IgnoreAndPopulate = 3
-}
--- dist/models/AcceptedType.js
+++
@@ -1,3 +0,0 @@
-/* eslint-disable @typescript-eslint/no-invalid-void-type */
-/* eslint-disable @typescript-eslint/no-type-alias */
-export {};
